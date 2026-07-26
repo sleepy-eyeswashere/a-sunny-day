e4a752854716e92b09322e6618aa89de9b768fd3
sleepy-eyeswashere <setayeshvizheh@gmail.com>
2026-07-26 22:15:52 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,46 @@ function openLetter(){
 
     document.querySelector(".intro").style.display="none";
 
-    let loading = document.getElementById("loading");
+
+    let loading=document.getElementById("loading");
 
     loading.style.display="flex";
 
 
     setTimeout(function(){
 
-        loading.style.display="none";
 
-        document
-        .getElementById("letter")
-        .classList.add("show");
+        loading.style.opacity="0";
 
 
-        document
-        .getElementById("decorations")
-        .classList.add("show");
+        setTimeout(function(){
+
+            loading.style.display="none";
 
 
-    },3500);
+            document
+            .getElementById("letter")
+            .classList.add("show");
+
+
+        },800);
+
+
+    },4000);
+
+}
+
+function goBack(){
+
+    document
+    .getElementById("letter")
+    .classList.remove("show");
+
+
+    setTimeout(()=>{
+
+        document.querySelector(".intro").style.display="block";
+
+    },800);
 
 }
